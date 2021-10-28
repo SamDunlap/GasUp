39a38efed81d88e172736a6402b1d92b8d546a84
Samuel Dunlap <samrdunlap@gmail.com>
2021-10-28 13:22:28 -0400

    show components

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GasUp.Logic;
+using GasUp.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
@@ -13,6 +14,8 @@ namespace GasUp.Pages
         [Inject]
         IJSRuntime js { get; set; }
         string stuff { get; set; }
+
+        List<StationModel> Stations { get; set; } = new List<StationModel>();
 
         private static object[] data = { "https://thingproxy.freeboard.io/fetch/https://www.gasbuddy.com/gasprices/michigan/ann-arbor" };
 
@@ -29,7 +32,7 @@ namespace GasUp.Pages
             var x = input.Length;
             stuff = input;
             GasStationLogic test = new GasStationLogic();
-            test.GetStations(input);
+            Stations = test.GetStations(input);
         }
 
         public void CallUserTest()
